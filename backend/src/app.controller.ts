e165fee4ca6bf6f9a3539385f1518b795f38968d
dev <dev@example.com>
2022-10-28 09:50:55 -0700

    create API for getting data of a specific user (by UserId)

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('users')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -24,4 +24,30 @@ export class AppService {
       throw new ForbiddenException('API not available');
     }
   }
+
+  async getUserDetail(userid: string): Promise<any> {
+    try {
+      const user = await lastValueFrom(
+        this.httpService
+          .get(`${process.env.USERS_API}/${userid}.json`, {
+            headers: {
+              Accept: 'application/json',
+            },
+          })
+          .pipe(map((response) => response.data)),
+      );
+
+      const { pull_requests } = user;
+
+      const sorted_desc_pull_request = pull_requests.sort(
+        (pullA: any, pullB: any) =>
+          new Date(pullB.created_at).valueOf() -
+          new Date(pullA.created_at).valueOf(),
+      );
+      user.pull_requests = sorted_desc_pull_request;
+      return user;
+    } catch (error) {
+      throw new ForbiddenException('API not available');
+    }
+  }
 }
