16f995dc17f771e52b002e39844011ee942b29ca
dev <dev@example.com>
2022-10-28 09:11:08 -0700

    Design HomeScreen, Display all users on homeScreen each in a seperate component UserCard

---
+++ frontend/components/UserCard.js
@@ -0,0 +1,42 @@
+import { Linking } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import { Text, Flex, Box } from "native-base";
+
+const UserCard = ({ name, profile }) => {
+  const navigation = useNavigation();
+
+  return (
+    <Box
+      rounded="lg"
+      bg="gray.200"
+      m="2"
+      p="4"
+      borderBottomWidth="1"
+      shadow="6"
+      borderColor="gray.400"
+    >
+      <TouchableOpacity
+      >
+        <Text fontWeight="medium" fontSize={20} color="#0c4a6e" py="1">
+          {name}
+        </Text>
+      </TouchableOpacity>
+      <Flex direction="row" mb="2.5" mt="1.5">
+        <Text fontSize={15} color="#78716c" bold>
+          Github Link:
+        </Text>
+        <Text
+          fontSize={15}
+          color="gray.400"
+          onPress={() => Linking.openURL(`${profile}`)}
+        >
+          {profile}
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default UserCard;
--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "native-base";
+import { Center, Heading, Box } from "native-base";
+import UserCard from "../components/UserCard";
+
+const list= require('../data.json')
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
 
-export default HomeScreen
+  return (
+    <SafeAreaView>
+      <Center>
+        <Heading fontWeight="medium" color="#00CCBB" py="4" bold italic>
+          GitHub Users
+        </Heading>
+      </Center>
+      <Box
+        shadow="2"
+        rounded="lg"
+        w={{ base: "full", md: "full", lg: "lg" }}
+        _light={{ bg: "coolGray.100" }}
+        _dark={{ bg: "gray.700" }}
+      >
+        <ScrollView>
+          {list?.map((user) => (
+            <UserCard
+              key={user.id}
+              name={user.nickname}
+              profile={user.github_profile}
+            />
+          ))}
+        </ScrollView>
+      </Box>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
