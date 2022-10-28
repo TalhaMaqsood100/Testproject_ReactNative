8e39a74d7ae2ee4d8a1c2d7df19e46a8537f81b4
dev <dev@example.com>
2022-10-28 09:48:13 -0700

    create API to get all Users

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsers(): any {
+    return this.appService.getUsers();
+  }
+
+  @Get(':userid')
+  getUserDetail(@Param('userid') userid: string): any {
+    return this.appService.getUserDetail(userid);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom, map } from 'rxjs';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  async getUsers(): Promise<any> {
+    try {
+      const users = await lastValueFrom(
+        this.httpService
+          .get(`${process.env.USERS_API}.json`, {
+            headers: {
+              Accept: 'application/json',
+            },
+          })
+          .pipe(map((response) => response.data)),
+      );
+
+      return users.slice(0, 10);
+    } catch (error) {
+      throw new ForbiddenException('API not available');
+    }
   }
 }
