2f2a7db982c18b29a96ba5aeffdfc071d5d350a4
dev <dev@example.com>
2022-10-28 09:03:48 -0700

    Create Screens ( HomeScreen, UserScreen) and Stack them in App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  NativeBaseProvider,
+  extendTheme,
+
+} from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createStackNavigator();
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+export const theme = extendTheme({ config });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
